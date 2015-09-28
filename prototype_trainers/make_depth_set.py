91aaad7d4586c1d25032df8df4fbf17fa077f512
David Frank <dquinnfrank@gmail.com>
2015-09-28 10:25:40 -0700

    Saving bug fix

--- prototype_trainers/make_depth_set.py
+++ prototype_trainers/make_depth_set.py
@@ -72,8 +72,8 @@ for item_name in image_pickle_names:
 	data_processed, label_processed = im_p.depth_difference_set(data_batch, label_batch, rand_features, verbose = True)
 
 	# Save the set
-	pickle.dump(data_processed, open(set_name + "_" + str(running_index).zfill(5) + "_data.p", 'wb', protocol=2))
-	pickle.dump(label_processed, open(set_name + "_" + str(running_index).zfill(5) + "_label.p", 'wb', protocol=2))
+	pickle.dump(data_processed, open(set_name + "_" + str(running_index).zfill(5) + "_data.p", 'wb'), protocol=2)
+	pickle.dump(label_processed, open(set_name + "_" + str(running_index).zfill(5) + "_label.p", 'wb'), protocol=2)
 
 	# Increment running index
 	running_index += 1
