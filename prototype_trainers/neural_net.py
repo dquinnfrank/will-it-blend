4cf2b2b916b12576a09c155b63ef19eb10125fa0
dquinnfrank <dquinnfrank@gmail.com>
2015-10-01 13:09:32 -0700

    Random forest works and shows training progress

--- prototype_trainers/neural_net.py
+++ prototype_trainers/neural_net.py
@@ -12,3 +12,33 @@ sys.path.insert(0, "../data_generation")
 
 import post_process as pp
 im_p = pp.Image_processing
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
+from keras.optimizers import SGD
+
+class Neural_net:
+
+	def __init__(self):
+
+		pass
+
+	def train_batch(self):
+
+		pass
+
+	def train(self):
+
+		pass
+
+	def save_model(self):
+
+		pass
+
+	def predict(self):
+
+		pass
+
+if __name__ == "__main__":
+
+	pass
